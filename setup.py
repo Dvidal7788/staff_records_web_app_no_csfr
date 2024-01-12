8aeb3b8c87f6269ba0deb20446cc79b52133a45d
David Vidal <dvidal7788@gmail.com>
2024-01-12 13:39:30 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YourProjectName',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Flask-Session==0.4.0',
+        'Flask-Mail==0.9.1',
+        'Flask-WTF==0.15.1',
+        'Werkzeug==2.0.3',
+    ],
+)
