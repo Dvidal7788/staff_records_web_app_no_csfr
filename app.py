748c9c7d59063c6cd0dcc06d7e0d6dc40b9438a5
David Vidal <dvidal7788@gmail.com>
2024-01-18 15:55:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ def index():
     print('\n\nDEBUG Index app.config["WTF_CSRF_SECRET_KEY"]:\n\n', app.config['WTF_CSRF_SECRET_KEY'])
 
     if not 'username' in session:
-        return render_template('index.html', session=session)
+        return render_template('index.html')
     else:
         username = session['username']
         return render_template('index.html', username=username)
