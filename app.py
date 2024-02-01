0f445f753329b3d956ecd77ed1c8f076b66e28a4
David Vidal <dvidal7788@gmail.com>
2024-02-01 16:48:54 -0500

    Removed csrf

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request, session
 from flask_session import Session
 from flask_mail import Mail, Message
-from flask_wtf import CSRFProtect, FlaskForm
+# from flask_wtf import CSRFProtect, FlaskForm
 from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
 import sqlite3
@@ -20,17 +20,17 @@ app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
-# Generate a random SECRET_KEY
-if 'SECRET_KEY' not in app.config:
-    app.config['SECRET_KEY'] = os.urandom(24).hex()
+# # Generate a random SECRET_KEY
+# if 'SECRET_KEY' not in app.config:
+#     app.config['SECRET_KEY'] = os.urandom(24).hex()
 
-# Generate a random WTF_CSRF_SECRET_KEY
-if 'WTF_CSRF_SECRET_KEY' not in app.config:
-    app.config['WTF_CSRF_SECRET_KEY'] = os.urandom(24).hex()
+# # Generate a random WTF_CSRF_SECRET_KEY
+# if 'WTF_CSRF_SECRET_KEY' not in app.config:
+#     app.config['WTF_CSRF_SECRET_KEY'] = os.urandom(24).hex()
 
-# Configure CSRF
-csrf = CSRFProtect(app)
-csrf.init_app(app)
+# # Configure CSRF
+# csrf = CSRFProtect(app)
+# csrf.init_app(app)
 
 # Configure e-mail
 app.config['MAIL_DEFAULT_SENDER'] = 'staffrecordsdatabase@gmail.com'
